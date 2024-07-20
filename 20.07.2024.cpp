4c9e055b2d87f9b1c9dd052e60e68c9803503ea9
Ayandip <137981575+Ayandip244@users.noreply.github.com>
2024-07-20 12:07:09 +0530

    Remove Half Nodes

---
+++ 20.07.2024.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    Node *RemoveHalfNodes(Node *root) {
+        // code here
+        if(root==NULL){
+            return NULL;
+        }
+        //recursive case
+        root->left=RemoveHalfNodes(root->left);
+        root->right=RemoveHalfNodes(root->right);
+
+        if(root->left== NULL && root->right!=NULL){
+           return root->right;
+        }
+        if(root->left!= NULL && root->right==NULL){
+            return root->left;
+        }
+        return root;
+    }
+};

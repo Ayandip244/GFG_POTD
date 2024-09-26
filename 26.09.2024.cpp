73fea0ba3f663fefeff20675826379527ea0819f
Ayandip <137981575+Ayandip244@users.noreply.github.com>
2024-09-26 17:30:33 +0530

    Roof Top

---
+++ 26.09.2024.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    // Function to find maximum number of consecutive steps
+    // to gain an increase in altitude with each step.
+    int maxStep(vector<int>& arr) {
+        // Your code here
+        int consecutive_steps=0,maxi=0;
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]>arr[i-1]){
+                consecutive_steps++;
+                maxi=max(maxi,consecutive_steps);
+            }
+            else{
+                consecutive_steps=0;
+            }
+        }
+        return maxi;
+    }
+};

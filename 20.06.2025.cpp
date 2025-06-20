7e05e6d6976d2f17d383f3d171e1a33e446c3798
Ayandip <137981575+Ayandip244@users.noreply.github.com>
2025-06-20 10:14:12 +0530

    Group Balls by Sequence

---
+++ 20.06.2025.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    bool validgroup(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == -1) {
+                continue;
+            }
+            int curr  = arr[i];
+            int count = 0;
+            for (int j = i; j < n; j++) {
+                if (arr[j] == curr) {
+                    curr++;
+                    count++;
+                    arr[j] = -1;
+                }
+                if (count == k) {
+                    break;
+                }
+            }
+            if (count != k) return false;
+        }
+        return true;
+    }
+};
